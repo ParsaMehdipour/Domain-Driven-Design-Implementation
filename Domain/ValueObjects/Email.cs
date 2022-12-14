c1b27075236eb0b1b7339913ccc842ec365b13cb
Parsa <Mehdipourparsa@gmail.com>
2022-12-14 14:25:03 +0330

    feat : add email value object

---
+++ Domain/ValueObjects/Email.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Primitives;
+
+namespace Domain.ValueObjects;
+
+public class Email : ValueObject
+{
+    private Email(string value)
+    {
+        Value = value;
+    }
+
+    public string Value { get; }
+
+
+    public override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Value;
+    }
+}
