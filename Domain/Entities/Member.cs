29e5dc1d3ee9366e0f465e6cf130b4c8ad80684c
Parsa <Mehdipourparsa@gmail.com>
2022-12-14 14:07:48 +0330

    feat : add member domain model

---
+++ Domain/Entities/Member.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Primitives;
+
+namespace Domain.Entities;
+
+public class Member : BaseEntity
+{
+    public Member(Guid id) : base(id)
+    {
+    }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+}
