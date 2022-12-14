7521ac145ee12af3556b9ca671c5ea1c07e6cb54
Parsa <Mehdipourparsa@gmail.com>
2022-12-14 14:25:16 +0330

    feat : add first name value object

---
+++ Domain/ValueObjects/FirstName.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Primitives;
+using Domain.Shared;
+
+namespace Domain.ValueObjects;
+
+public sealed class FirstName : ValueObject
+{
+    public const int MaxLength = 50;
+
+    public FirstName(string value)
+    {
+        Value = value;
+    }
+
+    public string Value { get; }
+
+    public static Result<FirstName> Create(string firstName)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+        {
+            return Result.Failure<FirstName>(new Error(
+                "FirstName.Empty",
+                "First name is empty."));
+        }
+
+        if (firstName.Length > MaxLength)
+        {
+            return Result.Failure<FirstName>(new Error(
+                "FirstName.TooLong",
+                "First name is too long."));
+        }
+
+        return new FirstName(firstName);
+    }
+
+    public override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Value;
+    }
+}
