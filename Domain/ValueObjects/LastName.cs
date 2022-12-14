9b99bc956d5fb9a13bd1ba9f25ebdac20c94e20b
Parsa <Mehdipourparsa@gmail.com>
2022-12-14 14:25:29 +0330

    feat : add last name value object

---
+++ Domain/ValueObjects/LastName.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Primitives;
+
+namespace Domain.ValueObjects;
+
+public class LastName : ValueObject
+{
+    private LastName(string value)
+    {
+        Value = value;
+    }
+
+    public string Value { get; }
+
+
+    public override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Value;
+    }
+}
