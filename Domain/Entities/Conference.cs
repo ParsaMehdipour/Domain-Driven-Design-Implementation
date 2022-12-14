ee967863f1992094dd7f9f6d9dac5eaf79964a17
Parsa <Mehdipourparsa@gmail.com>
2022-12-14 14:08:00 +0330

    feat : add conference entity

---
+++ Domain/Entities/Conference.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Primitives;
+
+namespace Domain.Entities;
+
+public class Conference : AggregateRoot
+{
+    public Conference(Guid id) : base(id)
+    {
+    }
+
+#nullable enable
+    public Member Creator { get; private set; }
+
+    public string Name { get; private set; }
+
+    public DateTime ScheduledAtUtc { get; private set; }
+
+    public string? Location { get; private set; }
+
+    public int? MaximumNumberOfAttendees { get; private set; }
+
+    public DateTime? InvitationsExpireAtUtc { get; private set; }
+
+    public int NumberOfAttendees { get; private set; }
+#nullable disable
+}
