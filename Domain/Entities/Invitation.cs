47ff286e27058025eac390f79af4ee74a72782d8
Parsa <Mehdipourparsa@gmail.com>
2022-12-18 23:28:25 +0330

    feat : add invitation entity

---
+++ Domain/Entities/Invitation.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Enums;
+using Domain.Primitives;
+
+namespace Domain.Entities;
+
+public class Invitation : BaseEntity
+{
+    internal Invitation(Guid id, Member member, Conference conference)
+        : base(id)
+    {
+        MemberId = member.Id;
+        ConferenceId = conference.Id;
+        Status = InvitationStatus.Pending;
+        CreatedOnUtc = DateTime.UtcNow;
+    }
+
+    public Guid ConferenceId { get; private set; }
+
+    public Guid MemberId { get; private set; }
+
+    public InvitationStatus Status { get; private set; }
+
+    public DateTime CreatedOnUtc { get; private set; }
+
+    public DateTime? ModifiedOnUtc { get; private set; }
+}
