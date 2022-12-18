36be2d1c68cb1a28eced2b51b57a540862844500
Parsa <Mehdipourparsa@gmail.com>
2022-12-18 23:28:14 +0330

    feat : add attendee entity

---
+++ Domain/Entities/Attendee.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Primitives;
+
+namespace Domain.Entities;
+
+public class Attendee : BaseEntity
+{
+
+    public Attendee(Guid id) : base(id)
+    {
+        CreatedOnUtc = DateTime.UtcNow;
+    }
+
+    public Guid ConferenceId { get; private set; }
+
+    public Guid MemberId { get; private set; }
+
+    public DateTime CreatedOnUtc { get; private set; }
+}
