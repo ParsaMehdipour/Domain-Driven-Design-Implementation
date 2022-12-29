98a7c89c45c7a8508ff166a0ddb29856b7b89666
Parsa <Mehdipourparsa@gmail.com>
2022-12-29 11:04:21 +0330

    refactor : finlizing conference entity

--- Domain/Entities/Conference.cs
+++ Domain/Entities/Conference.cs
@@ -1,26 +1,46 @@
-﻿using Domain.Primitives;
+﻿using Domain.Enums;
+using Domain.Primitives;
 
 namespace Domain.Entities;
 
 public class Conference : AggregateRoot
 {
-    public Conference(Guid id) : base(id)
+    private readonly List<Invitation> _invitations = new();
+    private readonly List<Attendee> _attendees = new();
+
+    private Conference(
+        Guid id,
+        Member creator,
+        ConferenceType type,
+        DateTime scheduledAtUtc,
+        string name,
+        string location)
+        : base(id)
     {
+        Creator = creator;
+        Type = type;
+        ScheduledAtUtc = scheduledAtUtc;
+        Name = name;
+        Location = location;
     }
 
-#nullable enable
     public Member Creator { get; private set; }
+
+    public ConferenceType Type { get; private set; }
 
     public string Name { get; private set; }
 
     public DateTime ScheduledAtUtc { get; private set; }
 
-    public string? Location { get; private set; }
+    public string Location { get; private set; }
 
     public int? MaximumNumberOfAttendees { get; private set; }
 
     public DateTime? InvitationsExpireAtUtc { get; private set; }
 
     public int NumberOfAttendees { get; private set; }
-#nullable disable
+
+    public IReadOnlyCollection<Attendee> Attendees => _attendees;
+
+    public IReadOnlyCollection<Invitation> Invitations => _invitations;
 }
