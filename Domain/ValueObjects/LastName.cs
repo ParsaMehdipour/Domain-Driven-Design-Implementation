232b8a4a67d435a02f18f908a9cc2106bfdc500c
Parsa <Mehdipourparsa@gmail.com>
2022-12-31 23:38:21 +0330

    refactor : add validation for last name

--- Domain/ValueObjects/LastName.cs
+++ Domain/ValueObjects/LastName.cs
@@ -1,9 +1,12 @@
-﻿using Domain.Primitives;
+﻿using Domain.Errors;
+using Domain.Primitives;
+using Domain.Shared;
 
 namespace Domain.ValueObjects;
 
 public class LastName : ValueObject
 {
+    public const int MaxLength = 50;
     private LastName(string value)
     {
         Value = value;
@@ -11,6 +14,20 @@ public class LastName : ValueObject
 
     public string Value { get; }
 
+    public static Result<LastName> Create(string lastName)
+    {
+        if (string.IsNullOrWhiteSpace(lastName))
+        {
+            return Result.Failure<LastName>(DomainErrors.LastName.Empty);
+        }
+
+        if (lastName.Length > MaxLength)
+        {
+            return Result.Failure<LastName>(DomainErrors.LastName.TooLong);
+        }
+
+        return new LastName(lastName);
+    }
 
     public override IEnumerable<object> GetAtomicValues()
     {
